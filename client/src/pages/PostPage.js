501bf55d2cfd58c78cf21dc2495d50561825e41d
Abdul <lhlrahman@gmail.com>
2023-11-16 00:25:02 -0500

    prepare for deployment

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -35,7 +35,7 @@ export default function PostPage() {
         </div>
       )}
       <div className="image">
-        <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
+        <img src={`https://habibs-blog.vercel.app/${postInfo.cover}`} alt=""/>
       </div>
       <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
     </div>
